c5a4618520da60fee949e4416d0de63b241dd20c
MelodyArthur <st20299917@outlook.cardiffmet.ac.uk>
2025-10-30 16:58:00 +0000

    Section 2

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -4,6 +4,7 @@
 
 
 int main(void) {
+	const float pi = 3.14159265359f;
 
 	// Initialise the engine (create window, setup OpenGL backend)
 	int initResult = engineInit("GDV4002 - Applied Maths for Games", 1024, 1024);
@@ -18,6 +19,15 @@ int main(void) {
 	//
 	// Setup game scene objects here
 	//
+	addObject("player",//defines the player object
+		glm::vec2(1.0f, 1.0f),//initial position
+		45.0f * pi / 180.0f,//initial rotation in radians
+		glm::vec2(0.5f, 1.0f),//changes the size of the object
+		"Resources\\Textures\\mcblock01.png",//adds a texture to the object
+		TextureProperties::NearestFilterTexture());//changes the texture properties
+	
+		
+
 	
 
 	// Enter main loop - this handles update and render calls

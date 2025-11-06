0c53c783a711b1a515206c852eed765dac84b125
MelodyArthur <st20299917@outlook.cardiffmet.ac.uk>
2025-11-06 17:08:32 +0000

    section 3

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -19,13 +19,29 @@ int main(void) {
 	//
 	// Setup game scene objects here
 	//
-	addObject("player",//defines the player object
+	GameObject2D* player1Object = getObject("player1");//retrieves the player object
+	if (player1Object != nullptr) 
+	{
+		printf("Player1 position: x=%f, y=%f\n", player1Object->position.x, player1Object->position.y);
+	}
+	addObject("player1",//defines the player object
 		glm::vec2(1.0f, 1.0f),//initial position
 		45.0f * pi / 180.0f,//initial rotation in radians
 		glm::vec2(0.5f, 1.0f),//changes the size of the object
 		"Resources\\Textures\\mcblock01.png",//adds a texture to the object
 		TextureProperties::NearestFilterTexture());//changes the texture properties
-	
+
+
+	addObject("player2");
+	GameObject2D* player2Object = getObject("player2");//retrieves the player2 object
+	if (player2Object != nullptr) {
+		printf("Player2 position: x=%f, y=%f\n", player2Object->position.x, player2Object->position.y);
+		player2Object->textureID = loadTexture("Resources\\Textures\\bumblebee.png");//adds a texture to player2
+	}
+	player2Object->position = glm::vec2(-1.0f, 1.0f);//changes the position of player2
+	player2Object->orientation = -45.0f * pi / 180.0f;//changes the rotation of player2
+	player2Object->size = glm::vec2(0.5f, 1.0f);//changes the size of player2
+
 		
 
 	

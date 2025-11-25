b609d9734754a89bbd36bf5259a2d6065199c8bd
MelodyArthur <st20299917@outlook.cardiffmet.ac.uk>
2025-11-25 12:29:48 +0000

    set up player charachter

--- gdv4002-base1/gdv4002-base1/Player.h
+++ gdv4002-base1/gdv4002-base1/Player.h
@@ -1,17 +1,14 @@
-#pragma once//never delete this line... EVER!
+#pragma once
 
 #include "GameObject2D.h"
 
 class Player : public GameObject2D {
 
 private:
-	float		mass;
-	glm::vec2	velocity;
-
-
+	float playerSpeed;
 
 public:
-	Player(glm::vec2 initPosition, float initOrientation, glm::vec2 initSize, GLuint initTextureID, float mass);
+	Player(glm::vec2 initPosition, float initOrientation, glm::vec2 initSize, GLuint initTextureID, float initialPlayerSpeed);
 
 	void update(double tDelta) override;
 
--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -1,3 +1,102 @@
-//hello
+#include "Player.h"
+#include "Engine.h"
+#include "Keys.h" // setup in chapter 6
+#include <bitset>
+
+// Function prototypes
+void myKeyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods); // setup in chapter 6
+
+
+// Globals
+std::bitset<5> keys{ 0x0 };
+
+
+
+int main(void) {
+
+	// Initialise the engine (create window, setup OpenGL backend)
+	int initResult = engineInit("GDV4002 - Applied Maths for Games", 512, 512, 5.0f);
+
+	// If the engine initialisation failed report error and exit
+	if (initResult != 0) {
+
+		printf("Cannot setup game window!!!\n");
+		return initResult; // exit if setup failed
+	}
+
+	// Setup game objects
+	// Create a player object and add it to the engine
+	GLuint playerTexture = loadTexture("Resources\\Textures\\player1_ship.png");
+	Player* mainPlayer = new Player(glm::vec2(-1.5f, 0.0f), 0.0f, glm::vec2(0.5f, 0.5f), playerTexture, 1.0f);
+	addObject("player", mainPlayer);
+
+
+	// Create an enemy object and add it to the engine
+
+	// Setup event handlers
+	setKeyboardHandler(myKeyboardHandler);
+
+
+	// Enter main loop - this handles update and render calls
+	engineMainLoop();
+
+	// When we quit (close window for example), clean up engine resources
+	engineShutdown();
+
+	// return success :)
+	return 0;
+}
+
+void myKeyboardHandler(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	// Check if the key was just pressed
+	if (action == GLFW_PRESS) {
+
+		// now check which key was pressed...
+		switch (key)
+		{
+		case GLFW_KEY_ESCAPE:
+			// If escape is pressed tell GLFW we want to close the window(and quit)
+			glfwSetWindowShouldClose(window, true);
+			break;
+		case GLFW_KEY_W:
+			keys[Key::W] = true;
+			break;
+		case GLFW_KEY_A:
+			keys[Key::A] = true;
+			break;
+		case GLFW_KEY_S:
+			keys[Key::S] = true;
+			break;
+		case GLFW_KEY_D:
+			keys[Key::D] = true;
+			break;
+		}
+		
+
+	}
+	// If not pressed, check the key has just been released
+	else if (action == GLFW_RELEASE) 
+	{
+		switch (key)
+		{
+		case GLFW_KEY_W:
+			keys[Key::W] = false;
+			break;
+		case GLFW_KEY_A:
+			keys[Key::A] = false;
+			break;
+		case GLFW_KEY_S:
+			keys[Key::S] = false;
+			break;
+		case GLFW_KEY_D:
+			keys[Key::D] = false;
+			break;
+		}
+
+	}
+}
+
+
 
 
